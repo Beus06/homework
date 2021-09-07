e68768dbbe95d72b7809cd06b9dd3cc209851efc
Beus06 <kerteszbeata9@gmail.com>
2021-09-07 12:41:01 +0200

    add package

---
+++ src/main/java/application/Main.java
@@ -0,0 +1,25 @@
+package application;
+
+import application.database.DBEngine;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        DBEngine dbEngine = new DBEngine();
+
+        if(dbEngine.isConnected()) {
+            
+            System.out.println(dbEngine.listAllMembers());
+
+        } else {
+            System.out.println("no connection");
+        }
+
+
+
+    }
+
+
+}
---
+++ src/main/java/application/tables/Days.java
@@ -0,0 +1,13 @@
+package application.tables;
+
+public enum Days {
+
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+
+}
